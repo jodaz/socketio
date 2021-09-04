ea5e46a68cd8e69a34224401827e025500179842
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-09-04 13:41:03 -0400

    Fix indentation

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,21 @@ import socketIOClient from 'socket.io-client';
 const ENDPOINT = 'http://127.0.0.1:4001';
 
 function App() {
-				const [response, setResponse] = useState("");
+	const [response, setResponse] = useState("");
 				
-				useEffect(() => {
-								const socket = socketIOClient(ENDPOINT);
-								socket.on("FromAPI", data => {
-												console.log(data);
-												setResponse(data);
-								})
-				}, []);
-				
-				return (
-								<p>
-												It's <time dateTime={response}>{response}</time>
-								</p>
-				);
+	useEffect(() => {
+		const socket = socketIOClient(ENDPOINT);
+		socket.on("FromAPI", data => {
+			console.log(data);
+			setResponse(data);
+		})
+	}, []);
+	
+	return (
+		<p>
+			It's <time dateTime={response}>{response}</time>
+		</p>
+	);
 }
 
 export default App;
